e1fc0cd27801936fea8ca46db03bcf97f055847c
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:08:37 +0900

    이력서 조회 관련 test 추가

--- src/test/java/com/example/api/board/controller/BoardControllerTest.java
+++ src/test/java/com/example/api/board/controller/BoardControllerTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
-class BoardControllerTest {
-  
+package com.example.api.board.controller;
+
+import com.example.api.board.controller.domain.Board;
+import com.example.api.board.controller.domain.CategoryDTO;
+import com.example.api.board.controller.domain.MyInfoDTO;
+import com.example.api.global.BaseIntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BoardControllerTest extends BaseIntegrationTest {
+    @Autowired BoardController boardController;
+
+    @Test
+    void getBoard(){
+        List<CategoryDTO> expectedCategoryDTOList = new ArrayList<>();
+        expectedCategoryDTOList.add(new CategoryDTO(category1.getCategoryId(), category1.getCategoryName()));
+        expectedCategoryDTOList.add(new CategoryDTO(category2.getCategoryId(), category2.getCategoryName()));
+
+        Board expectedBoard = new Board(
+                new MyInfoDTO(
+                "John Doe",
+                "Johnny",
+                30,
+                "Male",
+                "johndoe@example.com",
+                "123-456-7890",
+                innerCareerList,
+                externalCareerList,
+                possibleBoardList,
+                flavoredCategoryList,
+                3.5f,
+                3
+                ),
+                expectedCategoryDTOList
+        );
+
+        Board boardByEmployeeId = boardController.findBoardByEmployeeId(1L);
+
+        Assertions.assertEquals(expectedBoard, boardByEmployeeId);
+    }
 }
