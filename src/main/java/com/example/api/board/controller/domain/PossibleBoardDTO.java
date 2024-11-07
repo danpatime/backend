2de1b3cd4769f9890317116e916c8c513e8da584
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:04:27 +0900

    controller 전용ã DTO, BoardController 추가

--- src/main/java/com/example/api/board/controller/domain/PossibleBoardDTO.java
+++ src/main/java/com/example/api/board/controller/domain/PossibleBoardDTO.java
@@ -1,4 +1,17 @@
 package com.example.api.board.controller.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class PossibleBoardDTO {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
