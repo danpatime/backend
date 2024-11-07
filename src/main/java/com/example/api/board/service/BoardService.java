6b46a84e0385bf9510c5d9136cb7f51e416810c4
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:05:25 +0900

    BoardService, CategoryService 추가

--- src/main/java/com/example/api/board/service/BoardService.java
+++ src/main/java/com/example/api/board/service/BoardService.java
@@ -1,7 +1,8 @@
 package com.example.api.board.service;
 
+
 import com.example.api.board.controller.domain.MyInfoDTO;
-import com.example.api.board.repository.BoardRepository;
+import com.example.api.board.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,22 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository boardRepository;
+    private final MyInfoRepository myInfoRepository;
+    private final OfferEmploymentRepository offerEmploymentRepository;
+    private final ExternalCareerRepository externalCareerRepository;
+    private final PossibleBoardRepository possibleBoardRepository;
+    private final FlavoredRepository flavoredRepository;
 
-    public MyInfoDTO findMyInfoById(long id) {
-        return boardRepository.findMyInfoDTOById(id);
+    public MyInfoDTO findMyInfoById(long employeeId) {
+        MyInfoDTO myInfoDTOById = myInfoRepository.findMyInfoDTOById(employeeId);
+        myInfoDTOById.setInnerCarrerList(offerEmploymentRepository.findAllInnerCareerDTOByEmployeeId(employeeId));
+        myInfoDTOById.setExternalCareerList(externalCareerRepository.findAllByEmployeeEmployeeId(employeeId));
+        myInfoDTOById.setPossibleBoardList(possibleBoardRepository.findAllPossibleBoardByEmployeeEmployeeId(employeeId));
+        myInfoDTOById.setFlavoredCategoryList(flavoredRepository.findAllFlavoredByEmployeeId(employeeId));
+
+        return myInfoDTOById;
     }
+
+
 }
+
