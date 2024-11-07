d8ec87e8305f9323ac8ed23d89ce3246af4679ec
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:04:58 +0900

    board 관련 repository 추가

--- src/main/java/com/example/api/board/repository/OfferEmploymentRepository.java
+++ src/main/java/com/example/api/board/repository/OfferEmploymentRepository.java
@@ -1,4 +1,20 @@
 package com.example.api.board.repository;
 
-public interface OfferEmploymentRepository {
+import com.example.api.board.controller.domain.InnerCareerDTO;
+import com.example.api.domain.OfferEmployment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OfferEmploymentRepository extends JpaRepository<OfferEmployment, Long> {
+    @Query("select new com.example.api.board.controller.domain.InnerCareerDTO(b.businessName, c.contractStartTime, b.representationName, r) from OfferEmployment o " +
+            "join Contract c on o.suggestId = c.contractId " +
+            "join Business b on o.business.businessId = b.businessId "+
+            "join Review r on o.suggestId = r.suggestId " +
+            "where o.employee.employeeId = :employeeId")
+    List<InnerCareerDTO> findAllInnerCareerDTOByEmployeeId(@Param("employeeId") long employeeId);
 }
--- src/main/java/com/example/api/board/repository/PossibleBoardRepository.java
+++ src/main/java/com/example/api/board/repository/PossibleBoardRepository.java
@@ -1,4 +1,17 @@
 package com.example.api.board.repository;
 
-public interface PossibleBoardRepository {
+import com.example.api.board.controller.domain.PossibleBoardDTO;
+import com.example.api.domain.PossibleBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PossibleBoardRepository extends JpaRepository<PossibleBoard, Long> {
+    @Query("select new com.example.api.board.controller.domain.PossibleBoardDTO(p.startTime, p.endTime) " +
+            "from PossibleBoard p where p.employee.employeeId = :employeeId")
+    List<PossibleBoardDTO> findAllPossibleBoardByEmployeeEmployeeId(@Param("employeeId")Long employeeId);
 }
