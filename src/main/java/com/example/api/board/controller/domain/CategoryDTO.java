41ba8d1c0b3b0a31c2f43867edc0017e0e5631e8
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:04:27 +0900

    controller 전용ã DTO, BoardController 추가

--- src/main/java/com/example/api/board/controller/domain/CategoryDTO.java
+++ src/main/java/com/example/api/board/controller/domain/CategoryDTO.java
@@ -1,12 +1,14 @@
 package com.example.api.board.controller.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class CategoryDTO {
     private Long categoryId;
     private String categoryName;
--- src/main/java/com/example/api/board/controller/domain/MyInfoDTO.java
+++ src/main/java/com/example/api/board/controller/domain/MyInfoDTO.java
@@ -3,9 +3,17 @@ package com.example.api.board.controller.domain;
 import com.example.api.domain.Category;
 import com.example.api.domain.ExternalCareer;
 import com.example.api.domain.PossibleBoard;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+@Setter
 public class MyInfoDTO {
     private String name;
     private String nickname;
@@ -13,10 +21,21 @@ public class MyInfoDTO {
     private String sex;
     private String email;
     private String phone;
-    private List<InnerCarrer> innerCarrerList;
-    private List<ExternalCareer> externalCareerList;
-    private List<PossibleBoard> possibleBoardList;
-    private List<Category> flavoredCategoryList;
+    private List<InnerCareerDTO> innerCarrerList;
+    private List<ExternalCareerDTO> externalCareerList;
+    private List<PossibleBoardDTO> possibleBoardList;
+    private List<CategoryDTO> flavoredCategoryList;
     private float starPoint;
     private int workCount;
+
+    public MyInfoDTO(String name, String nickname, int age, String sex, String email, String phone, float starPoint, int workCount) {
+        this.name = name;
+        this.nickname = nickname;
+        this.age = age;
+        this.sex = sex;
+        this.email = email;
+        this.phone = phone;
+        this.starPoint = starPoint;
+        this.workCount = workCount;
+    }
 }
