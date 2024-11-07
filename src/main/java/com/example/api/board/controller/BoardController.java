5505cb603f3918d8369c11559e7cf4032cad2e5f
taeyeongKims <xodud8530@gmail.com>
2024-11-07 22:04:27 +0900

    controller 전용ã DTO, BoardController 추가

--- src/main/java/com/example/api/board/controller/BoardController.java
+++ src/main/java/com/example/api/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.example.api.board.controller;
 
 import com.example.api.board.controller.domain.CategoryDTO;
-import com.example.api.board.controller.domain.FormDTO;
+import com.example.api.board.controller.domain.Board;
 import com.example.api.board.controller.domain.MyInfoDTO;
 import com.example.api.board.service.BoardService;
 import com.example.api.board.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +18,10 @@ public class BoardController {
     private final BoardService boardService;
     private final CategoryService categoryService;
 
-    public FormDTO findById(long id) {
-        MyInfoDTO myInfoById = boardService.findMyInfoById(id);
-        List<CategoryDTO> categoryList = categoryService.findAll();
-        return new FormDTO(myInfoById, categoryList);
+    @GetMapping("/api/v1/possible-board/form/{employeeId}")
+    public Board findBoardByEmployeeId(@PathVariable("employeeId") Long employeeId) {
+        MyInfoDTO myInfoById = boardService.findMyInfoById(employeeId);
+        List<CategoryDTO> categoryList = categoryService.getAllCategories();
+        return new Board(myInfoById, categoryList);
     }
 }
