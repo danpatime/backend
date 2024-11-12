d59e62a0f241752ad2b6eacff35a95a88ed88a29
huhdy32 <mong3268@gmail.com>
2024-11-12 18:07:49 +0900

    #9 feat(ApiExceptionHandler): 어플리케이션 비즈니스 로직 실행 중 발생하는 예외에 대해 총괄적으로 처리하는 로직 구현

---
+++ src/main/java/com/example/api/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.api.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "-000", "서버 에러");
+
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String errorDescription;
+
+    ErrorCode(HttpStatus httpStatus, String errorCodeResponse, String errorDescription) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCodeResponse;
+        this.errorDescription = errorDescription;
+    }
+}
