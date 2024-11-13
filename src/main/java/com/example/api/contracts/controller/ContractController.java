cb6b042ed5b29ef288a98f530c91ab8bc430f681
huhdy32 <mong3268@gmail.com>
2024-11-13 14:43:14 +0900

    #6 feat(ContractService): 계약 업데이트 기능 구현

--- src/main/java/com/example/api/contracts/controller/ContractController.java
+++ src/main/java/com/example/api/contracts/controller/ContractController.java
@@ -1,12 +1,11 @@
 package com.example.api.contracts.controller;
 
-import com.example.api.contracts.dto.ContractDTO;
 import com.example.api.contracts.ContractService;
-import com.example.api.contracts.dto.AcceptContractCommand;
 import com.example.api.contracts.dto.AcceptSuggestCommand;
-import com.example.api.contracts.dto.QueryAllSuggestsForMeCommand;
-import com.example.api.contracts.dto.SuggestedBusinessResponse;
 import com.example.api.contracts.dto.UpdateContractConditionCommand;
+import com.example.api.contracts.dto.QueryAllSuggestsForMeCommand;
+import java.time.LocalDateTime;
+import com.example.api.contracts.dto.SuggestedBusinessResponse;
 import com.example.api.contracts.dto.UpdateContractConditionRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,26 @@ class ContractController {
         final AcceptSuggestCommand acceptSuggestCommand = new AcceptSuggestCommand(suggestId);
         contractService.acceptSuggest(acceptSuggestCommand);
         return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/api/v1/contracts/{contractId}")
+    public ResponseEntity<?> updateContractCondition(
+            @PathVariable(required = true) final Long contractId,
+            @RequestBody final UpdateContractConditionRequest updateContractConditionRequest
+    ) {
+        final UpdateContractConditionCommand updateCommand = updateContractConditionRequest.toCommand(contractId);
+        contractService.updateContract(updateCommand);
+        return ResponseEntity.ok(null);
+    }
+
+    record UpdateContractConditionRequest(
+            LocalDateTime suggestStartDateTime,
+            LocalDateTime suggestEndDateTime,
+            Integer suggestHourlyPayment
+    ) {
+        UpdateContractConditionCommand toCommand(final Long contractId) {
+            return new UpdateContractConditionCommand(contractId, this.suggestStartDateTime, this.suggestEndDateTime, this.suggestHourlyPayment);
+        }
     }
 }
 
