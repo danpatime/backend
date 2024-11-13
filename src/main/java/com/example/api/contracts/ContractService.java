13ae5abc15ba709d8661d5cecb6bd5ed73fbcaad
huhdy32 <mong3268@gmail.com>
2024-11-13 13:23:27 +0900

    #6 feat(ContractService): 알바생 제안 수락 기능 구현

--- src/main/java/com/example/api/contracts/ContractService.java
+++ src/main/java/com/example/api/contracts/ContractService.java
@@ -1,7 +1,10 @@
 package com.example.api.contracts;
 
+import com.example.api.contracts.dto.AcceptSuggestCommand;
 import com.example.api.contracts.dto.QueryAllSuggestsForMeCommand;
 import com.example.api.contracts.dto.SuggestedBusiness;
+import com.example.api.domain.Contract;
+import com.example.api.domain.OfferEmployment;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ContractService {
     private final OfferRepository offerRepository;
+    private final ContractRepository contractRepository;
+    private final ContractMapper contractMapper;
 
     @Transactional(readOnly = true)
     public List<SuggestedBusiness> getAllRelatedSuggests(final QueryAllSuggestsForMeCommand allSuggestsForMeCommand) {
         return offerRepository.queryEmployersSuggests(allSuggestsForMeCommand.employeeId());
+    }
+
+    @Transactional
+    public void acceptSuggest(final AcceptSuggestCommand acceptSuggestCommand) {
+        final OfferEmployment offerEmployment = offerRepository.findById(acceptSuggestCommand.suggestId())
+                .orElseThrow();
+        offerEmployment.succeeded();
+
+        final Contract contract = contractMapper.notYetSucceeded(offerEmployment);
+        contractRepository.save(contract);
     }
 }
--- src/main/java/com/example/api/domain/Contract.java
+++ src/main/java/com/example/api/domain/Contract.java
@@ -2,23 +2,21 @@ package com.example.api.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "CONTRACT")
+@NoArgsConstructor
 public class Contract extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contractId;
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "SUGGEST_ID")
     private OfferEmployment offerEmployment;
-
     @Column(name = "CONTRACT_START_TIME")
     private LocalDateTime contractStartTime;
     @Column(name = "CONTRACT_END_TIME")
@@ -27,5 +25,22 @@ public class Contract extends BaseEntity {
     private int contractHourlyPay;
     @Column(name = "CONTRACT_SUCCEDED", columnDefinition = "boolean DEFAULT false")
     private boolean contractSucceeded;
+<<<<<<< HEAD
+=======
+
+    public Contract(
+            final OfferEmployment offerEmployment,
+            final LocalDateTime contractStartTime,
+            final LocalDateTime contractEndTime,
+            final int contractHourlyPay,
+            final boolean contractSucceeded
+    ) {
+        this.offerEmployment = offerEmployment;
+        this.contractStartTime = contractStartTime;
+        this.contractEndTime = contractEndTime;
+        this.contractHourlyPay = contractHourlyPay;
+        this.contractSucceeded = contractSucceeded;
+    }
+>>>>>>> 9fd56a2 (#6 feat(ContractService): 알바생 제안 수락 기능 구현)
 }
 
