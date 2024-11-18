7728077eacc77704ac95369983d612878e4f4489
taeyeongKims <xodud8530@gmail.com>
2024-11-18 20:12:22 +0900

    #1 @Trasactional 위치를 각 메소드마다 선언하는 걸로 변경

--- src/main/java/com/example/api/board/service/CategoryService.java
+++ src/main/java/com/example/api/board/service/CategoryService.java
@@ -3,19 +3,19 @@ package com.example.api.board.service;
 import com.example.api.board.controller.domain.response.CategoryDTO;
 import com.example.api.domain.repository.CategoryRepository;
 import com.example.api.domain.Category;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
         return categories.stream()
