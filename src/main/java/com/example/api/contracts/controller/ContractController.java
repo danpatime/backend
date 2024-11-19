03e15d8a914842d9eb9a89551c279cf2c50b5275
taeyeongKims <xodud8530@gmail.com>
2024-11-19 22:04:44 +0900

    #26 요청 수락 서비스에 채팅방 생성 로직 추가

--- src/main/java/com/example/api/contracts/controller/ContractController.java
+++ src/main/java/com/example/api/contracts/controller/ContractController.java
@@ -11,13 +11,7 @@ import com.example.api.contracts.dto.UpdateContractConditionRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,9 +30,8 @@ class ContractController {
 
     @PostMapping("/api/v1/contracts/suggests/{suggestId}/accept")
     public ResponseEntity<?> acceptContractContact(
-            @PathVariable(required = true) final Long suggestId
+            @RequestBody final AcceptSuggestCommand acceptSuggestCommand
     ) {
-        final AcceptSuggestCommand acceptSuggestCommand = new AcceptSuggestCommand(suggestId);
         contractService.acceptSuggest(acceptSuggestCommand);
         return ResponseEntity.ok(null);
     }
