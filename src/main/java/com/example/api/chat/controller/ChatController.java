25cb4af04db818bf045c16057132a1497bc1b64c
taeyeongKims <xodud8530@gmail.com>
2024-11-20 00:19:11 +0900

    #27 채팅 전송, 읽기, 채팅방 리스트 조회(채팅 축약 정보), 페이징 채팅 요청 기능 추가

--- src/main/java/com/example/api/chat/controller/ChatController.java
+++ src/main/java/com/example/api/chat/controller/ChatController.java
@@ -1,11 +1,18 @@
 package com.example.api.chat.controller;
 
 import com.example.api.chat.controller.dto.request.ChatSendRequest;
+import com.example.api.chat.controller.dto.request.ReadRequest;
+import com.example.api.chat.controller.dto.request.UserIdRequest;
+import com.example.api.chat.controller.dto.response.ChatSummaryResponse;
 import com.example.api.chat.service.ChatService;
+import com.example.api.domain.Chat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,5 +22,21 @@ public class ChatController {
     @MessageMapping("/send")
     public void sendMessage(@Payload ChatSendRequest chatRequest) {
         chatService.sendChat(chatRequest);
+    }
+
+    @MessageMapping("/read")
+    public void readMessage(@Payload ReadRequest readRequest) {
+        chatService.readChats(readRequest);
+    }
+
+    @GetMapping("/chat/summaries")
+    public ResponseEntity<ChatSummaryResponse> getChatSummaries(@RequestBody UserIdRequest userIdRequest) {
+        return ResponseEntity.ok(chatService.getChatSummaries(userIdRequest));
+    }
+
+    @GetMapping("/chat/room/{roomId}/chats")
+    public ResponseEntity<List<Chat>> getMessages(@PathVariable("roomId") Long chatRoomId,
+                                                   @RequestParam(value = "lastChatId", required = false) String chatId) {
+        return ResponseEntity.ok(chatService.getChats(chatRoomId,chatId));
     }
 }
