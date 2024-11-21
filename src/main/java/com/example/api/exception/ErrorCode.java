414a5f846283c2f5f075509377e9360324eed9a5
yuna83 <b1a4coco@gmail.com>
2024-11-21 21:41:00 +0900

    #2 feat(RegisterService): 회원가입 구현

--- src/main/java/com/example/api/exception/ErrorCode.java
+++ src/main/java/com/example/api/exception/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "-000", "서버 에러");
+    SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "-000", "서버 에러"),
+    DUPLICATE_LOGIN_ID(HttpStatus.BAD_REQUEST, "-001", "중복된 ID입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
@@ -17,3 +18,4 @@ public enum ErrorCode {
         this.errorDescription = errorDescription;
     }
 }
+
