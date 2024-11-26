d8eaae36a7d555a09ced41cdf0f73b465c0ea51d
yuna83 <b1a4coco@gmail.com>
2024-11-27 02:26:41 +0900

    #38 feat(InquiryService): 문의 생성, 조회 구현

---
+++ src/main/java/com/example/api/inquiry/InquiryRepository.java
@@ -0,0 +1,9 @@
+package com.example.api.inquiry;
+
+import com.example.api.domain.Inquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+    List<Inquiry> findByCreatedBy(String createdBy);
+}
