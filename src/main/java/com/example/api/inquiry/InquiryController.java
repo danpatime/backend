cbdaa06f30c48f2d5d195411c6022291fea556a7
yuna83 <b1a4coco@gmail.com>
2024-11-28 01:43:44 +0900

    #38 refactor(InquiryService):Model Mapper 추가

--- src/main/java/com/example/api/inquiry/InquiryController.java
+++ src/main/java/com/example/api/inquiry/InquiryController.java
@@ -5,6 +5,7 @@ import com.example.api.domain.Inquiry;
 import com.example.api.inquiry.dto.RequestDTO;
 import com.example.api.inquiry.dto.ResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/support")
 public class InquiryController {
     private final InquiryService inquiryService;
+    private final ModelMapper modelMapper;
 
 
     @PostMapping("/inquiry")
-    public ResponseEntity<ResponseDTO> createInquiry(@RequestBody RequestDTO RequestDTO) {
-        Inquiry inquiry = inquiryService.saveInquiry(RequestDTO);
+    public ResponseEntity<ResponseDTO> createInquiry(@RequestBody RequestDTO requestDTO) {
+        Inquiry inquiry = inquiryService.saveInquiry(requestDTO);
         ResponseDTO responseDTO = mapToResponseDTO(inquiry);
         return ResponseEntity.ok(responseDTO);
     }
@@ -35,29 +37,7 @@ public class InquiryController {
         return ResponseEntity.ok(responseDTOs);
     }
 
-
-    @PutMapping("/inquiry/{inquiryId}/response")
-    public ResponseEntity<ResponseDTO> updateResponseStatus(
-            @PathVariable Long inquiryId,
-            @RequestParam int responseStatus,
-            @RequestParam String processStatus) {
-
-        Inquiry inquiry = inquiryService.updateResponseStatus(inquiryId, responseStatus, processStatus);
-        ResponseDTO responseDTO = mapToResponseDTO(inquiry);
-        return ResponseEntity.ok(responseDTO);
-    }
-
     private ResponseDTO mapToResponseDTO(Inquiry inquiry) {
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setInquiryId(inquiry.getInquiryId());
-        responseDTO.setInquiryType(inquiry.getInquiryType());
-        responseDTO.setSubInquiryType(inquiry.getSubInquiryType());
-        responseDTO.setTitle(inquiry.getTitle());
-        responseDTO.setContent(inquiry.getContent());
-        responseDTO.setResponseStatus(inquiry.getResponseStatus());
-        responseDTO.setProcessStatus(inquiry.getProcessStatus());
-        responseDTO.setAnswerDate(inquiry.getAnswerDate());
-        responseDTO.setCreatedBy(inquiry.getCreatedBy());
-        return responseDTO;
+        return modelMapper.map(inquiry, ResponseDTO.class);
     }
 }
