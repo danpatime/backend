27fdc0ff8bc176fe9d3cd3cca7b29cb7d5521422
taeyeongkims <xodud8530@gmail.com>
2024-12-01 23:23:57 +0900

    #2 이메ì(¼, 보안 설정 추가

---
+++ src/main/java/com/example/api/global/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.api.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/v1/auth/register").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.defaultSuccessUrl("/").permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
