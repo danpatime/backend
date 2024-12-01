9fb824ccb2971310734f0297f7a35154a5c5e866
taeyeongkims <xodud8530@gmail.com>
2024-12-01 23:30:37 +0900

    #2 코드 생성기, 코드 도메인 추가

---
+++ src/main/java/com/example/api/account/domain/Code.java
@@ -0,0 +1,25 @@
+package com.example.api.account.domain;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter
+@Document(collection = "code")
+public class Code {
+    @Id
+    private String id;
+    private String email;
+    private String code;
+    @Indexed(expireAfterSeconds = 600)
+    private Date createdAt;
+
+    public Code(String email, String code) {
+        this.email = email;
+        this.code = code;
+        this.createdAt = new Date();
+    }
+}
