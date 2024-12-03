86d00e4678ecc8f9715af8b4266244fd6b3570bb
taeyeongKims <xodud8530@gmail.com>
2024-12-03 20:17:07 +0900

    #2 EmailConfig 설정과 속성을 분리

--- src/main/java/com/example/api/global/config/EmailConfig.java
+++ src/main/java/com/example/api/global/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.example.api.global.config;
 
+import com.example.api.global.properties.EmailProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,51 +11,20 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+    private final EmailProperties emailProperties;
 
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
-    @Value("${spring.mail.protocol}")
-    private String protocol;
-
-    @Value("${spring.mail.properties.mail.smtp.auth}")
-    private boolean auth;
-
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-    private boolean starttlsEnable;
-
-    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
-    private boolean starttlsRequired;
-
-    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
-    private int connectionTimeout;
-
-    @Value("${spring.mail.properties.mail.smtp.timeout}")
-    private int timeout;
-
-    @Value("${spring.mail.properties.mail.smtp.writetimeout}")
-    private int writeTimeout;
-
-    @Value("${spring.mail.auth-code-expiration-millis}")
-    private int expirationMillis;
+    public EmailConfig(EmailProperties emailProperties) {
+        this.emailProperties = emailProperties;
+    }
 
     @Bean
     public JavaMailSender javaMailSender() {
-        System.out.println("Configuring JavaMailSender with host: " + host);
+        System.out.println("Configuring JavaMailSender with host: " + emailProperties.getHost());
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost(emailProperties.getHost());
+        mailSender.setPort(emailProperties.getPort());
+        mailSender.setUsername(emailProperties.getUsername());
+        mailSender.setPassword(emailProperties.getPassword());
         mailSender.setDefaultEncoding("UTF-8");
         mailSender.setJavaMailProperties(getMailProperties());
 
@@ -63,13 +33,13 @@ public class EmailConfig {
 
     private Properties getMailProperties() {
         Properties properties = new Properties();
-        properties.put("mail.smtp.auth", auth);
-        properties.put("mail.smtp.starttls.enable", starttlsEnable);
-        properties.put("mail.smtp.starttls.required", starttlsRequired);
-        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
-        properties.put("mail.smtp.timeout", timeout);
-        properties.put("mail.smtp.writetimeout", writeTimeout);
-        properties.put("mail.smtp.expirationMillis", expirationMillis);
+        properties.put("mail.smtp.auth", emailProperties.isAuth());
+        properties.put("mail.smtp.starttls.enable", emailProperties.isStarttlsEnable());
+        properties.put("mail.smtp.starttls.required", emailProperties.isStarttlsRequired());
+        properties.put("mail.smtp.connectiontimeout", emailProperties.getConnectionTimeout());
+        properties.put("mail.smtp.timeout", emailProperties.getTimeout());
+        properties.put("mail.smtp.writetimeout", emailProperties.getWriteTimeout());
+        properties.put("mail.smtp.expirationMillis", emailProperties.getExpirationMillis());
 
         return properties;
     }
