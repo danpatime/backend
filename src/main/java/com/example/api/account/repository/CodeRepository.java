c051c5bd03a2f74e636623fcaaf6853792525de5
taeyeongKims <xodud8530@gmail.com>
2024-12-03 10:27:56 +0900

    #2 CodeRepository의 함수명 변경으로 인한 서비스 변경

--- src/main/java/com/example/api/account/repository/CodeRepository.java
+++ src/main/java/com/example/api/account/repository/CodeRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface CodeRepository extends MongoRepository<Code, String> {
-    Optional<Code> findCodeByEmail(String email);
+    Optional<Code> findFirstByEmailOrderByCreatedAtDesc(String email);
 }
