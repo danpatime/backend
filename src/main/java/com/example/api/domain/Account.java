f834c82d1b99cc1a5f004ef98b19286f5050424e
taeyeongKims <xodud8530@gmail.com>
2024-12-03 10:17:12 +0900

    #2 Account 도메인에 국적, 역할 필드 추가

--- src/main/java/com/example/api/domain/Account.java
+++ src/main/java/com/example/api/domain/Account.java
@@ -1,10 +1,11 @@
 package com.example.api.domain;
 
+import com.example.api.account.domain.Nationality;
+import com.example.api.account.domain.UserRole;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 
 @Entity
 @Getter
@@ -29,6 +30,12 @@ public class Account extends BaseEntity {
     private String phoneNumber;
     @Column(name = "ACCOUNT_EMAIL")
     private String email;
+    @Column(name = "ACCOUNT_NATIONALITY")
+    @Enumerated(EnumType.STRING)
+    private Nationality nationality;
+    @Column(name = "ACCOUNT_ROLE")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
     @Column(name = "ACCOUNT_SEX")
     private String sex;
     @Column(name = "ACCOUNT_AGE")
@@ -47,13 +54,15 @@ public class Account extends BaseEntity {
     public Account() {
     }
 
-    public Account(String loginId, String password, String name, String nickname, String phoneNumber, String email) {
+    public Account(String loginId, String password, String name, String nickname, String phoneNumber, String email, Nationality nationality, UserRole role) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.nickname = nickname;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        this.nationality = nationality;
+        this.role = role;
         this.starPoint =  0.0f;
         this.workCount = 0;
         this.openStatus = true;
