cf830f39ab6c969c669b33eec96b72ddb9f7570b
taeyeongKims <xodud8530@gmail.com>
2024-12-03 10:29:15 +0900

    #2 SecurityConfig에서 formLogin을 사용하지 않고, 비로그인 접근 허용 경로 추가

--- src/main/java/com/example/api/global/config/SecurityConfig.java
+++ src/main/java/com/example/api/global/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.example.api.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -15,10 +18,14 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers("/api/v1/auth/register").permitAll()
+                                .requestMatchers("/api/v1/account/**",
+                                        "/api/v1/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.defaultSuccessUrl("/").permitAll());
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults());
+
+
 
         return http.build();
     }
