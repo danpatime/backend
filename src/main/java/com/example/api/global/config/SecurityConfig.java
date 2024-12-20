ba1681b22635d3b14bd2ef598c913d130f65355a
taeyeongKims <xodud8530@gmail.com>
2024-12-21 00:14:15 +0900

    #12 (SecurityConfig) 시큐리티 설정 파일

--- src/main/java/com/example/api/global/config/SecurityConfig.java
+++ src/main/java/com/example/api/global/config/SecurityConfig.java
@@ -1,37 +1,59 @@
 package com.example.api.global.config;
 
+import com.example.api.auth.entitiy.JwtAuthenticationProvider;
+import com.example.api.global.config.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .requestMatchers("/api/v1/account/**",
-                                        "/api/v1/**").permitAll()
-                                .anyRequest().authenticated()
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/api/v1/account/**",
+                                "/api/v1/**"
+                        ).permitAll()
+                        .anyRequest().hasRole("EMPLOYEE")
                 )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(withDefaults());
-
-
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable());
 
         return http.build();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(List.of(jwtAuthenticationProvider));
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 }
