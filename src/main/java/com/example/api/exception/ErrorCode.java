58232eb9ad0b4c14035571d84311be39aef41be1
taeyeongKims <xodud8530@gmail.com>
2024-12-21 00:17:48 +0900

    #12 (ErrorCode) 로그인 및 토큰 관련 에러코드 추가

--- src/main/java/com/example/api/exception/ErrorCode.java
+++ src/main/java/com/example/api/exception/ErrorCode.java
@@ -10,6 +10,17 @@ public enum ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "-101", "이미 가입된 이메일입니다."),
     INCORRECT_CODE(HttpStatus.BAD_REQUEST, "-103", "이메일 인증에 실패하였습니다."),
     EXPIRATION_DATE_END(HttpStatus.BAD_REQUEST, "-104", "코드의 유효 기간이 만료 되었습니다."),
+    NULL_USER(HttpStatus.BAD_REQUEST, "-105", "존재하지 않는 회원입니다."),
+    DELETED_USER(HttpStatus.BAD_REQUEST, "-106", "탈퇴한 회원입니다."),
+    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, "-107", "틀린 비밀번호입니다."),
+
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "-T1", "올바르지 않은 AccessToken입니다."),
+    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "-T2", "만료된 AccessToken입니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "T3", "만료된 ReFreshToken"),
+    NULL_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,"T4", "존재하지 않은 ReFreshToken 접근"),
+    NOT_ACCESS_TOKEN_FOR_REISSUE(HttpStatus.BAD_REQUEST,"T5","재발급하기에는 유효기간이 남은 AccessToken"),
+    TOKEN_MISSING_AUTHORITY(HttpStatus.BAD_REQUEST,"T6","권한 정보가 담겨있지 않은 토큰입니다."),
+
 
     FAIL_GENERATE_CODE(HttpStatus.INTERNAL_SERVER_ERROR, "-500", "코드 생성에 실패하였습니다."),
     FAIL_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "-501", "이메일 전송에 실패하였습니다."),
