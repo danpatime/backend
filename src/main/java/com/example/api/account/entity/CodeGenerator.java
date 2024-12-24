892767d339c6018ff2e3ff26d834137c1ba5740b
taeyeongkims <xodud8530@gmail.com>
2024-12-24 16:51:48 +0900

    #12 CodeGenerator Component로 등록

--- src/main/java/com/example/api/account/entity/CodeGenerator.java
+++ src/main/java/com/example/api/account/entity/CodeGenerator.java
@@ -2,19 +2,26 @@ package com.example.api.account.entity;
 
 import com.example.api.exception.BusinessException;
 import com.example.api.exception.ErrorCode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
 
+@Component
 public class CodeGenerator {
-    public static String generateCode() {
-        int length = 6;
+    @Value("${code.length}")
+    private int length;
+    @Value("${code.digit_range}")
+    private int digitRange;
+
+    public String generateCode() {
         try {
             Random random = SecureRandom.getInstanceStrong();
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < length; i++) {
-                builder.append(random.nextInt(10));
+                builder.append(random.nextInt(digitRange));
             }
             return builder.toString();
         } catch (NoSuchAlgorithmException e) {
