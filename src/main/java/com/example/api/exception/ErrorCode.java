766f0bf9fa5a8cbdcf79359806d4acc6629ed9ff
taeyeongkims <xodud8530@gmail.com>
2024-12-28 12:51:22 +0900

    #50 (ErrorCode) oauth2 관련 에러코드 추가

--- src/main/java/com/example/api/exception/ErrorCode.java
+++ src/main/java/com/example/api/exception/ErrorCode.java
@@ -13,6 +13,8 @@ public enum ErrorCode {
     NULL_USER(HttpStatus.BAD_REQUEST, "-105", "존재하지 않는 회원입니다."),
     DELETED_USER(HttpStatus.BAD_REQUEST, "-106", "탈퇴한 회원입니다."),
     INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, "-107", "틀린 비밀번호입니다."),
+    INCORRECT_DATA(HttpStatus.BAD_REQUEST, "-108", "올바르지 않은 정보입니다."),
+    INVALID_REDIRECT_URI(HttpStatus.BAD_REQUEST,"-109","유효하지 않은 REDIRECT URI입니다."),
 
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "-T1", "올바르지 않은 AccessToken입니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "-T2", "만료된 AccessToken입니다."),
@@ -20,7 +22,6 @@ public enum ErrorCode {
     NULL_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,"T4", "존재하지 않은 ReFreshToken 접근"),
     NOT_ACCESS_TOKEN_FOR_REISSUE(HttpStatus.BAD_REQUEST,"T5","재발급하기에는 유효기간이 남은 AccessToken"),
     TOKEN_MISSING_AUTHORITY(HttpStatus.BAD_REQUEST,"T6","권한 정보가 담겨있지 않은 토큰입니다."),
-
 
     FAIL_GENERATE_CODE(HttpStatus.INTERNAL_SERVER_ERROR, "-500", "코드 생성에 실패하였습니다."),
     FAIL_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "-501", "이메일 전송에 실패하였습니다."),
