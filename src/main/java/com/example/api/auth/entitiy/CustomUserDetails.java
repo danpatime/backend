584bece6a879899b7150b62c1f9f91ca328dc236
taeyeongkims <xodud8530@gmail.com>
2024-12-28 12:45:16 +0900

    #50 (UserDetail) oauth2 로그인과 일반 로그인의 authentication 양식을 통일

--- src/main/java/com/example/api/auth/entitiy/CustomUserDetails.java
+++ src/main/java/com/example/api/auth/entitiy/CustomUserDetails.java
@@ -1,23 +1,48 @@
 package com.example.api.auth.entitiy;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
-public class CustomUserDetails{
+@Getter
+public class CustomUserDetails implements UserDetails, OAuth2User {
     private final Long userId;
+    private final String name;
+    private final String email;
     private final Collection<? extends GrantedAuthority> authorities;
 
-    public CustomUserDetails(final Long userId, final Collection<? extends GrantedAuthority> authorities) {
+    public CustomUserDetails(final Long userId, final String name, final String email, final Collection<? extends GrantedAuthority> authorities) {
         this.userId = userId;
+        this.name = name;
+        this.email = email;
         this.authorities = authorities;
     }
 
+    @Override
+    public Map<String, Object> getAttributes(){
+        return Map.of(
+                "userId", userId,
+                "name", name,
+                "email", email
+        );
+    }
+
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
 
-    public Long getUserId() {
-        return userId;
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
     }
 }
