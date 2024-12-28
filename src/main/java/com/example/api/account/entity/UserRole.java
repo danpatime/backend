c6d2ce767ca4ba4c15e3844646aac158eb2ea505
taeyeongkims <xodud8530@gmail.com>
2024-12-28 12:44:17 +0900

    #50 (UserRole) 문자열을 객체로 매핑시켜주는 @JsonCreator 메서드 추가

--- src/main/java/com/example/api/account/entity/UserRole.java
+++ src/main/java/com/example/api/account/entity/UserRole.java
@@ -1,6 +1,11 @@
 package com.example.api.account.entity;
 
-public enum UserRole {
+import com.example.api.exception.BusinessException;
+import com.example.api.exception.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
     EMPLOYEE(0, "알바생"),
     EMPLOYER(1, "사장");
 
@@ -19,6 +24,16 @@ public enum UserRole {
             }
         }
         return null;
+    }
+
+    @JsonCreator
+    public static UserRole from(String value) {
+        for (UserRole role : values()) {
+            if (role.name().equalsIgnoreCase(value)) {
+                return role;
+            }
+        }
+        throw new BusinessException(ErrorCode.INCORRECT_DATA);
     }
 
     public Integer getCode() {
