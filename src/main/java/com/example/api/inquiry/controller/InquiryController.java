6180cbd6e9de0b60e23e9e9ca57c6b4bea130755
yuna83 <b1a4coco@gmail.com>
2024-12-31 15:53:40 +0900

    #38 refactor(InquiryController): 컨트롤러 리팩토링

--- src/main/java/com/example/api/inquiry/controller/InquiryController.java
+++ src/main/java/com/example/api/inquiry/controller/InquiryController.java
@@ -1,44 +1,51 @@
 package com.example.api.inquiry.controller;
 
-
+import com.example.api.domain.Account;
 import com.example.api.domain.Inquiry;
 import com.example.api.inquiry.InquiryService;
 import com.example.api.inquiry.dto.InquiryRequest;
-import com.example.api.inquiry.dto.InquiryCommand;
+import com.example.api.inquiry.dto.InquiryResponse;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/support")
 public class InquiryController {
     private final InquiryService inquiryService;
-    private final ModelMapper modelMapper;
-
 
     @PostMapping("/inquiry")
-    public ResponseEntity<InquiryCommand> createInquiry(@RequestBody InquiryRequest requestDTO) {
-        Inquiry inquiry = inquiryService.saveInquiry(requestDTO);
-        InquiryCommand responseDTO = mapToResponseDTO(inquiry);
-        return ResponseEntity.ok(responseDTO);
+    public ResponseEntity<InquiryResponse> createInquiry(
+            @RequestBody final InquiryRequest inquiryRequest
+    ) {
+        final Inquiry inquiry = inquiryService.saveInquiry(
+                inquiryRequest,
+                inquiryRequest.createdBy()
+        );
+        final InquiryResponse inquiryResponse = mapToResponse(inquiry);
+        return ResponseEntity.ok(inquiryResponse);
     }
-
 
     @GetMapping("/my-inquiries")
-    public ResponseEntity<List<InquiryCommand>> getMyInquiries(@RequestParam String createdBy) {
-        List<Inquiry> inquiries = inquiryService.getInquiriesByUser(createdBy);
-        List<InquiryCommand> responseDTOs = inquiries.stream()
-                .map(this::mapToResponseDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(responseDTOs);
+    public ResponseEntity<List<InquiryResponse>> getMyInquiries(
+            @RequestParam(required = true) final Long accountId
+    ) {
+        final List<InquiryResponse> inquiryResponses = inquiryService.getInquiriesByAccountId(accountId);
+        return ResponseEntity.ok(inquiryResponses);
     }
 
-    private InquiryCommand mapToResponseDTO(Inquiry inquiry) {
-        return modelMapper.map(inquiry, InquiryCommand.class);
+    private InquiryResponse mapToResponse(Inquiry inquiry) {
+        return new InquiryResponse(
+                inquiry.getInquiryId(),
+                inquiry.getInquiryType(),
+                inquiry.getSubInquiryType(),
+                inquiry.getTitle(),
+                inquiry.getContent(),
+                inquiry.getInquiryStatus().name(),
+                inquiry.getAnswerDate(),
+                inquiry.getCreatedBy()
+        );
     }
 }
