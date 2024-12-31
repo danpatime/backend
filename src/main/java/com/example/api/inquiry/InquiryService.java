8973659615df5b360f5247a9ba141635ea11888d
yuna83 <b1a4coco@gmail.com>
2024-12-31 14:48:07 +0900

    #38 refactor(InquiryService): 서비스 리팩토링

--- src/main/java/com/example/api/inquiry/InquiryService.java
+++ src/main/java/com/example/api/inquiry/InquiryService.java
@@ -1,34 +1,63 @@
 package com.example.api.inquiry;
 
+import com.example.api.domain.Account;
 import com.example.api.domain.Inquiry;
+import com.example.api.inquiry.dto.InquiryCommand;
 import com.example.api.inquiry.dto.InquiryRequest;
+import com.example.api.inquiry.dto.InquiryResponse;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class InquiryService {
     private final InquiryRepository inquiryRepository;
-    private final ModelMapper modelMapper;
 
-    public Inquiry saveInquiry(InquiryRequest inquiryRequestDTO) {
-        Inquiry inquiry = modelMapper.map(inquiryRequestDTO, Inquiry.class);
-        inquiry.setInquiryStatus(Inquiry.InquiryStatus.WAITING);
-        inquiry.setProcessStatus("처리중");
-        inquiry.setAnswerDate(null);
-
+    @Transactional
+    public Inquiry saveInquiry(
+            @Validated final InquiryRequest inquiryRequest,
+            @Validated final Account account
+    ) {
+        final InquiryCommand command = inquiryRequest.toCommand(account);
+        final Inquiry inquiry = mapToInquiry(command);
         return inquiryRepository.save(inquiry);
     }
 
-
-    public List<Inquiry> getInquiriesByUser(String createdBy) {
-        return inquiryRepository.findByCreatedBy(createdBy);
+    @Transactional(readOnly = true)
+    public List<InquiryResponse> getInquiriesByUser(@Validated final Account account) {
+        final List<Inquiry> inquiries = inquiryRepository.findByCreatedBy(account);
+        return inquiries.stream()
+                .map(this::mapToInquiryResponse)
+                .collect(Collectors.toList());
     }
 
+    private Inquiry mapToInquiry(@Validated final InquiryCommand command) {
+        final Inquiry inquiry = new Inquiry();
+        inquiry.setInquiryType(command.inquiryType());
+        inquiry.setSubInquiryType(command.subInquiryType());
+        inquiry.setTitle(command.title());
+        inquiry.setContent(command.content());
+        inquiry.setInquiryStatus(Inquiry.InquiryStatus.valueOf(command.inquiryStatus()));
+        inquiry.setAnswerDate(command.answerDate());
+        inquiry.setCreatedBy(command.createdBy());
+        return inquiry;
+    }
+
+    private InquiryResponse mapToInquiryResponse(@Validated final Inquiry inquiry) {
+        return new InquiryResponse(
+                inquiry.getInquiryId(),
+                inquiry.getInquiryType(),
+                inquiry.getSubInquiryType(),
+                inquiry.getTitle(),
+                inquiry.getContent(),
+                inquiry.getInquiryStatus().name(),
+                inquiry.getAnswerDate(),
+                inquiry.getCreatedBy()
+        );
+    }
 }
-
-
-
