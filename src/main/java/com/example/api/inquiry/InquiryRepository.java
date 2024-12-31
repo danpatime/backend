d1ec1e916087c7aa189ce13efac79e672fef553d
yuna83 <b1a4coco@gmail.com>
2024-12-31 14:48:07 +0900

    #38 refactor(InquiryService): 서비스 리팩토링

--- src/main/java/com/example/api/inquiry/InquiryRepository.java
+++ src/main/java/com/example/api/inquiry/InquiryRepository.java
@@ -1,9 +1,10 @@
 package com.example.api.inquiry;
 
+import com.example.api.domain.Account;
 import com.example.api.domain.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
-    List<Inquiry> findByCreatedBy(String createdBy);
+    List<Inquiry> findByCreatedBy(Account createdBy);
 }
--- src/main/java/com/example/api/inquiry/dto/InquiryRequest.java
+++ src/main/java/com/example/api/inquiry/dto/InquiryRequest.java
@@ -1,13 +1,28 @@
 package com.example.api.inquiry.dto;
 
+import com.example.api.domain.Account;
 import org.springframework.lang.NonNull;
 
 public record InquiryRequest(
+        @NonNull
         String inquiryType,
         String subInquiryType,
         String title,
         String content,
         @NonNull
-        Long accountId
+        Account createdBy
 ) {
+
+    public InquiryCommand toCommand(final Account account) {
+        return new InquiryCommand(
+                null,
+                inquiryType,
+                subInquiryType,
+                title,
+                content,
+                "WAITING",
+                null,
+                account
+        );
+    }
 }
