467bd03cc1132192970d047a505c6c2c88133372
taeyeongkims <xodud8530@gmail.com>
2025-01-03 22:13:37 +0900

    #54 (Repository) AuthRepository를 AccountRepository로 병합 및 LocationRepository 생성

--- src/main/java/com/example/api/account/repository/LocationRepository.java
+++ src/main/java/com/example/api/account/repository/LocationRepository.java
@@ -1,4 +1,9 @@
 package com.example.api.account.repository;
 
-public interface LocationRepository {
+import com.example.api.account.entity.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
 }
