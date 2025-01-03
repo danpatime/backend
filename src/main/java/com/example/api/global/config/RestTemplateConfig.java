fb024d4d014809e8b84390613b597ef425ffb3e0
taeyeongkims <xodud8530@gmail.com>
2025-01-03 22:16:44 +0900

    #54 (RestTemplateConfig) RestTemplate 빈 등록

--- src/main/java/com/example/api/global/config/RestTemplateConfig.java
+++ src/main/java/com/example/api/global/config/RestTemplateConfig.java
@@ -1,4 +1,23 @@
 package com.example.api.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory clientHttpRequestFactory) {
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결 타임아웃 5초
+        factory.setReadTimeout(5000);    // 읽기 타임아웃 5초
+        return factory;
+    }
 }
