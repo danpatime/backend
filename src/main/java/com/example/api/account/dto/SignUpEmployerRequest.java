c92461ab1a7103bd3a7a281e830ca77cf82e5f8a
taeyeongkims <xodud8530@gmail.com>
2025-01-03 22:08:12 +0900

    #54 (dto) 사업자 정보 검증 및 회원가입 dto 구현

--- src/main/java/com/example/api/account/dto/SignUpEmployerRequest.java
+++ src/main/java/com/example/api/account/dto/SignUpEmployerRequest.java
@@ -1,27 +1,34 @@
 package com.example.api.account.dto;
 
+import com.example.api.account.entity.Location;
 import com.example.api.account.entity.Nationality;
 import com.example.api.account.entity.UserRole;
 import com.example.api.global.config.resolver.ValidEmail;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record SignUpEmployeeRequest(
+public record SignUpEmployerRequest(
     @NotBlank
-    String loginId,
+    String loginId,    // 로그인 id
     @NotBlank
-    String password,
-    @NotBlank
-    String name,
-    @NotBlank
-    String nickname,
+    String password,    // 비밀번호
     @ValidEmail
-    String email,
-    @NotNull
-    Nationality nationality,
-    @NotNull
-    UserRole role,
+    String email,   // 이메일
     @NotBlank
-    String phoneNumber
+    String businessRegistrationNumber,  // 사업자 번호
+    @NotBlank
+    String businessName,    // 회사명
+    @NotBlank
+    String representationName,  // 대표명
+    @NotBlank
+    String businessOpenDate,    // 개업연월일
+    @NotNull
+    Location location,
+    @NotNull
+    Nationality nationality,    // 국적
+    @NotNull
+    UserRole role,     // 권한
+    @NotBlank
+    String phoneNumber   // 휴대폰 번호
 ) {
 }
