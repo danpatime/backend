b206e63fbdad4b59ae95a1595832d1535f1056b6
yuna83 <b1a4coco@gmail.com>
2025-01-05 01:37:56 +0900

    #53 refactor(ReviewCommand): dto 리팩토링

--- src/main/java/com/example/api/review/dto/ReviewCommand.java
+++ src/main/java/com/example/api/review/dto/ReviewCommand.java
@@ -1,19 +1,7 @@
 package com.example.api.review.dto;
 
-import java.time.LocalDateTime;
-
 public record ReviewCommand(
-        Long accountId,
-        String businessName,
-        Long businessId,
-        LocalDateTime contractSrartTime,
-        LocalDateTime contractEndTime,
-        int reviewStarPoint,
-        String reviewContent) {
-
-    public ReviewCommand(Long accountId) {
-        this(accountId, null, null, null, null, 0, null);
-    }
-}
+        Long accountId
+){}
 
 
