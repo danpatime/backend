67990354512fa3430c02b2281ae3e339716f7a65
taeyeongkims <xodud8530@gmail.com>
2025-01-09 11:44:12 +0900

    #66 (AccountRepository) 프로필 이미지 관련 쿼리 추가

--- src/main/java/com/example/api/account/repository/AccountRepository.java
+++ src/main/java/com/example/api/account/repository/AccountRepository.java
@@ -4,6 +4,8 @@ import com.example.api.domain.Account;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -16,4 +18,11 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByEmail(String email);
 
     Optional<Account> findUserByLoginId(String loginId);
+
+    @Query("SELECT a.profileImage FROM Account a WHERE a.accountId = :accountId")
+    Optional<String> findProfileImageByAccountId(@Param("accountId") Long accountId);
+
+    @Query("update Account a set a.profileImage = :profileImage where a.accountId = :accountId")
+    @Modifying
+    void updateProfileImageByAccountId(@Param("profileImage") String profileImage, @Param("accountId") Long accountId);
 }
