e17edfb70cb5c01bf8a06f48181abffd044c6b66
taeyeongkims <xodud8530@gmail.com>
2025-01-09 11:26:57 +0900

    #66 (dto) 필요 dto 수정

--- src/main/java/com/example/api/aws/dto/UploadProfileRequest.java
+++ src/main/java/com/example/api/aws/dto/UploadProfileRequest.java
@@ -1,4 +1,9 @@
 package com.example.api.aws.dto;
 
-public record UploadProfileRequest() {
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
+public record UploadProfileRequest(
+        @NotNull Long userId,
+        @NotNull MultipartFile multipartFile) {
 }
