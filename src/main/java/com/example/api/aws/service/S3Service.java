fa8c12e2eda2d36b431ea2d59335c7b24e60ba4f
taeyeongkims <xodud8530@gmail.com>
2025-01-09 11:27:48 +0900

    #66 (service) s3 service 구현

--- src/main/java/com/example/api/aws/service/S3Service.java
+++ src/main/java/com/example/api/aws/service/S3Service.java
@@ -1,84 +1,94 @@
-package com.example.api.aws.entity;
+package com.example.api.aws.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.api.account.repository.AccountRepository;
-import com.example.api.aws.dto.AwsS3;
+import com.example.api.aws.dto.S3UploadRequest;
+import com.example.api.aws.dto.OldKeyRequest;
 import com.example.api.aws.dto.UploadProfileRequest;
+import com.example.api.aws.dto.UploadProfileResponse;
 import com.example.api.exception.BusinessException;
 import com.example.api.exception.ErrorCode;
 import com.example.api.global.config.AmazonConfig;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class AwsUtils {
+@Slf4j
+public class S3Service {
     private final AmazonS3 amazonS3;
     private final AmazonConfig amazonConfig;
     private final AccountRepository accountRepository;
 
     @Transactional
-    public AwsS3 upload(@Validated UploadProfileRequest request) {
-        if (request.multipartFile() == null || request.multipartFile().isEmpty()) {
-            throw new BusinessException(ErrorCode.FILE_UPLOAD_FAILED);
-        }
+    public UploadProfileResponse upload(@Validated final UploadProfileRequest request) {
+        // 업로드 파일이 null 이라면 기본 프로필로 초기화
+        if (initDefaultIfFileIsNull(request)) return new UploadProfileResponse(null);
 
         Optional<String> userProfile = accountRepository.findProfileImageByAccountId(request.userId());
-        userProfile.ifPresent(oldKey -> remove(new AwsS3(oldKey, null)));
+        userProfile.ifPresent(oldKey -> remove(new OldKeyRequest(oldKey)));
 
-        String key = generateFileName(request.userId(), request.multipartFile());
-        String path = uploadToS3(request.multipartFile(), key);
-        accountRepository.updateProfileImageByAccountId(key, request.userId());
+        String key = generateFileName(request);
+        String path = uploadToS3(new S3UploadRequest(request.multipartFile(), key));
+        accountRepository.updateProfileImageByAccountId(key, request.userId());     // S3 업로드 이후 사용자 테이블 프로필 값 업데이트
 
-        return new AwsS3(key, path);
+        return new UploadProfileResponse(path);
     }
 
-    private String generateFileName(Long userId, MultipartFile file) {
-        String contentType = file.getContentType();
+    private boolean initDefaultIfFileIsNull(final UploadProfileRequest request) {
+        if (request.multipartFile() == null || request.multipartFile().isEmpty()) {
+            accountRepository.updateProfileImageByAccountId(null, request.userId());
+            String oldKey = "user-uploads/" + request.userId() + "/profile.png";
+            remove(new OldKeyRequest(oldKey));
+            return true;
+        }
+        return false;
+    }
+
+    private String generateFileName(final UploadProfileRequest request) {
+        String contentType = request.multipartFile().getContentType();
         String fileExtension = contentType != null && contentType.contains("/")
                 ? "." + contentType.split("/")[1]
                 : ".png";
-        return String.format("user-profile/%d/profile%s", userId, fileExtension);
+        return String.format("user-uploads/%d/profile%s", request.userId(), fileExtension);
     }
 
-    private String uploadToS3(MultipartFile uploadFile, String fileName) {
+    private String uploadToS3(final S3UploadRequest s3UploadRequest) {
         try {
             ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(uploadFile.getSize());
-            metadata.setContentType(uploadFile.getContentType());
+            metadata.setContentLength(s3UploadRequest.multipartFile().getSize());
+            metadata.setContentType(s3UploadRequest.multipartFile().getContentType());
 
             amazonS3.putObject(
                     new PutObjectRequest(
                             amazonConfig.getBucket(),
-                            fileName,
-                            uploadFile.getInputStream(),
+                            s3UploadRequest.key(),
+                            s3UploadRequest.multipartFile().getInputStream(),
                             metadata
                     )
             );
-            return getS3Url(amazonConfig.getBucket(), fileName);
+
+            return amazonS3.getUrl(amazonConfig.getBucket(), s3UploadRequest.key()).toString();
 
         } catch (IOException e) {
             throw new BusinessException(ErrorCode.FILE_UPLOAD_FAILED);
         }
     }
 
-    private String getS3Url(String bucket, String fileName) {
-        return amazonS3.getUrl(bucket, fileName).toString();
-    }
-
-    public void remove(AwsS3 awsS3) {
-        if (!amazonS3.doesObjectExist(amazonConfig.getBucket(), awsS3.key())) {
-            throw new AmazonS3Exception("Object " + awsS3.key() + " does not exist!");
+    public void remove(final OldKeyRequest request) {
+        if (!amazonS3.doesObjectExist(amazonConfig.getBucket(), request.oldKey())) {
+            throw new AmazonS3Exception("Object " + request.oldKey() + " does not exist!");
         }
-        amazonS3.deleteObject(amazonConfig.getBucket(), awsS3.key());
+        amazonS3.deleteObject(amazonConfig.getBucket(), request.oldKey());
     }
 }
