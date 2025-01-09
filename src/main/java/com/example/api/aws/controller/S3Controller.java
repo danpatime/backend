0c15c8945247bb9d053e7df2ab6ac05a7efd34c4
taeyeongkims <xodud8530@gmail.com>
2025-01-09 11:26:14 +0900

    #66 (S3Controller) 이미지 업로드 컨트롤러 구현

---
+++ src/main/java/com/example/api/aws/controller/S3Controller.java
@@ -0,0 +1,23 @@
+package com.example.api.aws.controller;
+
+import com.example.api.aws.dto.UploadProfileRequest;
+import com.example.api.aws.service.S3Service;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class S3Controller {
+    private final S3Service s3Service;
+
+    @PostMapping(value = "/upload/profile", consumes = "multipart/form-data")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+        UploadProfileRequest request = new UploadProfileRequest(1L, file);
+        return new ResponseEntity<>(s3Service.upload(request).path(), HttpStatus.OK);
+    }
+}
