37f1ba50232cfc0251e8818c3bb01b87749b4d4b
yuna83 <b1a4coco@gmail.com>
2025-01-10 17:36:50 +0900

    #73 feat(ReviewReport): 도메인 추가

---
+++ src/main/java/com/example/api/domain/ReviewReport.java
@@ -0,0 +1,38 @@
+package com.example.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "REVIEW_REPORT")
+public class ReviewReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "review_id", nullable = false)
+    private Long reviewId;
+
+    @Column(name = "employee_id", nullable = false)
+    private Long employeeId;
+
+    @Column(name = "reason", nullable = false)
+    private String reason;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/api/reviewreport/ReviewReportService.java
@@ -0,0 +1,4 @@
+package com.example.api.reviewreport;
+
+public class ReviewReportService {
+}
