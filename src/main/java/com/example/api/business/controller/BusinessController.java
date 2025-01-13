24f443c9e8c16a344bc817c072722da999c14676
huhdy32 <mong3268@gmail.com>
2025-01-13 10:39:53 +0900

    #78 feat: 가게 세부정보 조회 기능 구현

--- src/main/java/com/example/api/business/controller/BusinessController.java
+++ src/main/java/com/example/api/business/controller/BusinessController.java
@@ -1,16 +1,20 @@
 package com.example.api.business.controller;
 
+import com.example.api.business.BusinessQueryService;
 import com.example.api.business.BusinessService;
 import com.example.api.business.dto.AddBusinessCommand;
 import com.example.api.business.dto.ModifyBusinessCommand;
+import com.example.api.business.dto.QueryBusinessDetailCommand;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BusinessController {
     private final BusinessService businessService;
+    private final BusinessQueryService businessQueryService;
+
+    @GetMapping
+    public ResponseEntity<?> getMyBusiness(
+            @RequestParam final Long businessId
+    ) {
+        return ResponseEntity.ok(businessQueryService.loadDetails(new QueryBusinessDetailCommand(businessId)));
+    }
 
     @PutMapping
     public ResponseEntity<?> modifyMyBusiness(
