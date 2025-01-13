c5e7d75faf8a1d5d5808fa44f9b9a83749ccf11d
huhdy32 <mong3268@gmail.com>
2025-01-13 12:39:44 +0900

    #78 feat: 고용주가 작성한 리뷰 조회기능 구현

--- src/main/generated/com/example/api/domain/QReview.java
+++ src/main/generated/com/example/api/domain/QReview.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,9 +18,13 @@ public class QReview extends EntityPathBase<Review> {
 
     private static final long serialVersionUID = 731127133L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QReview review = new QReview("review");
 
     public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final QContract contract;
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
@@ -34,15 +39,24 @@ public class QReview extends EntityPathBase<Review> {
     public final DateTimePath<java.time.LocalDateTime> updatedDate = _super.updatedDate;
 
     public QReview(String variable) {
-        super(Review.class, forVariable(variable));
+        this(Review.class, forVariable(variable), INITS);
     }
 
     public QReview(Path<? extends Review> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QReview(PathMetadata metadata) {
-        super(Review.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReview(PathMetadata metadata, PathInits inits) {
+        this(Review.class, metadata, inits);
+    }
+
+    public QReview(Class<? extends Review> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.contract = inits.isInitialized("contract") ? new QContract(forProperty("contract"), inits.get("contract")) : null;
     }
 
 }
