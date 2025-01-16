e1c882e8a39fce843d4f21d45e62b833627f8953
taeyeongKims <xodud8530@gmail.com>
2025-01-16 16:28:31 +0900

    #80 business list 조회

--- src/main/java/com/example/api/business/BusinessRepository.java
+++ src/main/java/com/example/api/business/BusinessRepository.java
@@ -1,15 +1,14 @@
 package com.example.api.business;
 
 import com.example.api.domain.Business;
-
-import java.util.List;
-import java.util.Optional;
-
 import com.example.api.employer.controller.dto.EmployerBusinessesRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
