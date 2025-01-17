6d19f6c99ab2c37aeb92e4adb413496045410960
taeyeongKims <xodud8530@gmail.com>
2025-01-17 15:07:56 +0900

    #81 종료 요청에 따른 상태 변경 및 카운터 쿼리 repository에 추가

--- src/main/java/com/example/api/account/repository/AccountRepository.java
+++ src/main/java/com/example/api/account/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package com.example.api.account.repository;
 
 import com.example.api.domain.Account;
+import com.example.api.offeremployment.dto.StarPointAndWorkCountRequest;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.Optional;
 
@@ -22,7 +24,15 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     @Query("SELECT a.profileImage FROM Account a WHERE a.accountId = :accountId")
     Optional<String> findProfileImageByAccountId(@Param("accountId") Long accountId);
 
-    @Query("update Account a set a.profileImage = :profileImage where a.accountId = :accountId")
     @Modifying
+    @Query("update Account a set a.profileImage = :profileImage where a.accountId = :accountId")
     void updateProfileImageByAccountId(@Param("profileImage") String profileImage, @Param("accountId") Long accountId);
+
+    @Modifying
+    @Query("update Account a " +
+            "set a.starPoint = ((a.starPoint * a.workCount) + :newStarPoint) / (a.workCount+1), " +
+            "a.workCount = a.workCount + 1 " +
+            "where a.accountId in " +
+            "(select oe.employee.accountId From OfferEmployment oe where oe.suggestId = :suggestId)")
+    void updateWorkCountBySuggestId(@Param("suggestId") Long suggestId, @Param("starPoint") Integer newStarPoint);
 }
