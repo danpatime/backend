b216d79487dff353c55db2f982d405a5ad948f13
taeyeongKims <xodud8530@gmail.com>
2025-01-17 13:19:28 +0900

    #80 rebase develop

--- src/main/java/com/example/api/employer/controller/dto/EmployerBusinessesRequest.java
+++ src/main/java/com/example/api/employer/controller/dto/EmployerBusinessesRequest.java
@@ -1,9 +1,10 @@
 package com.example.api.employer.controller.dto;
 
+import com.example.api.account.entity.Location;
 import jakarta.validation.constraints.NotNull;
 
 public record EmployerBusinessesRequest(
         @NotNull String businessName,
-        @NotNull String businessLocation
+        @NotNull Location businessLocation
 ) {
 }
