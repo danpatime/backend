08ed2dd83644871b19f75f449290db8b7a93aed6
taeyeongKims <xodud8530@gmail.com>
2025-01-17 12:43:29 +0900

    #80 order by locationId

--- src/main/java/com/example/api/employer/controller/EmployerController.java
+++ src/main/java/com/example/api/employer/controller/EmployerController.java
@@ -1,6 +1,6 @@
 package com.example.api.employer.controller;
 
-import com.example.api.board.controller.domain.request.EmployeeIdRequest;
+import com.example.api.board.dto.request.EmployeeIdRequest;
 import com.example.api.employer.controller.dto.EmployerBusinessesRequest;
 import com.example.api.employer.controller.dto.LikeEmployeeDTO;
 import com.example.api.employer.service.EmployerService;
@@ -12,19 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/api/v1/employees/")
 @RequiredArgsConstructor
 public class EmployerController {
     private final EmployerService employerService;
 
-    @GetMapping("/api/v1/employees/favorites/{employerId}")
-    public ResponseEntity getLikeEmployee(@PathVariable Long employerId) {
+    @GetMapping("favorites/{employerId}")
+    public ResponseEntity getLikeEmployee(@PathVariable() Long employerId) {
         EmployeeIdRequest employeeIdRequest = new EmployeeIdRequest(employerId);
         List<LikeEmployeeDTO> result = employerService.getLikeEmployee(employeeIdRequest);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/api/v1/employees/businesses/{employerId}")
+    @GetMapping("businesses/{employerId}")
     public ResponseEntity getEmployeeBusinessList(@PathVariable Long employerId) {
         EmployeeIdRequest employeeIdRequest = new EmployeeIdRequest(employerId);
         List<EmployerBusinessesRequest> businesses = employerService.getEmployerBusinessList(employeeIdRequest);
