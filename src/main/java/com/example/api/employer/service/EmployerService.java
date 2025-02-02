bfb6c4d44df8a2a6791a02f661ecdd1268bd95c6
taeyeongkims <xodud8530@gmail.com>
2025-02-03 00:28:52 +0900

    scrap api 추가

--- src/main/java/com/example/api/employer/service/EmployerService.java
+++ src/main/java/com/example/api/employer/service/EmployerService.java
@@ -2,13 +2,17 @@ package com.example.api.employer.service;
 
 import com.example.api.account.repository.AccountRepository;
 import com.example.api.business.BusinessRepository;
+import com.example.api.domain.Scrap;
 import com.example.api.domain.repository.ExternalCareerRepository;
 import com.example.api.domain.repository.FlavoredCategoryRepository;
 import com.example.api.domain.Account;
 import com.example.api.employer.controller.dto.EmployerBusinessesRequest;
 import com.example.api.employer.controller.dto.EmployerIdRequest;
+import com.example.api.employer.controller.dto.FavoriteEmployeeRequest;
 import com.example.api.employer.controller.dto.LikeEmployeeDTO;
 import com.example.api.employer.repository.ScrapRepository;
+import com.example.api.global.exception.BusinessException;
+import com.example.api.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +29,7 @@ public class EmployerService {
     private final ExternalCareerRepository externalCareerRepository;
     private final FlavoredCategoryRepository flavoredRepository;
     private final BusinessRepository businessRepository;
+    private final AccountRepository accountRepository;
 
     @Transactional(readOnly = true)
     public List<LikeEmployeeDTO> getLikeEmployee(final EmployerIdRequest employerIdRequest) {
@@ -50,5 +55,25 @@ public class EmployerService {
     @Transactional(readOnly = true)
     public List<EmployerBusinessesRequest> getEmployerBusinessList(final EmployerIdRequest employerIdRequest) {
         return businessRepository.findBusinessesByEmployerId(employerIdRequest.employerId());
+    }
+
+    @Transactional
+    public String addFavoriteEmployee(final FavoriteEmployeeRequest favorite) {
+        Account employee = accountRepository.findById(favorite.employeeId()).orElseThrow(() -> new BusinessException(ErrorCode.NULL_USER));
+        Account employer = accountRepository.findById(favorite.employerId()).orElseThrow(() -> new BusinessException(ErrorCode.NULL_USER));
+        Scrap newScrap = new Scrap(employee, employer);
+        scrapRepository.save(newScrap);
+        return "성공적으로 즐겨찾기를 완료하였습니다.";
+    }
+
+    @Transactional
+    public String deleteFavoriteEmployee(final FavoriteEmployeeRequest favorite) {
+        Account employee = accountRepository.findById(favorite.employeeId()).orElseThrow(() -> new BusinessException(ErrorCode.NULL_USER));
+        Account employer = accountRepository.findById(favorite.employerId()).orElseThrow(() -> new BusinessException(ErrorCode.NULL_USER));
+
+        Scrap deleteScrap = scrapRepository.findFirstByEmployeeAndEmployer(employee, employer)
+                .orElseThrow(() -> new BusinessException(ErrorCode.NULL_USER));
+        scrapRepository.delete(deleteScrap);
+        return "성공적으로 즐겨찾기를 삭제하였습니다.";
     }
 }
